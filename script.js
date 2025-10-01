5cad61d7efde3556b70dd589793ed259a42a3df2
Shashank Singh <singshashank7@gmail.com>
2025-10-01 12:52:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.querySelectorAll('.truck-button').forEach(button => {
     button.addEventListener('click', e => {
 
         e.preventDefault();
-        
+         
         let box = button.querySelector('.box'),
             truck = button.querySelector('.truck');
         
